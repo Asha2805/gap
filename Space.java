4c04279e685c866a7895f5b2c398eb87d5abd13d
Asha <43291441+Asha2805@users.noreply.github.com>
2018-10-16 10:12:28 -0400

    Update and rename README.md to Space.java

---
+++ Space.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Space
+{
+public static void main(String[] args)
+{
+String q,a;
+Scanner w=new Scanner(System.in);
+q=w.nextLine();
+a=q.replaceAll("");
+System.out.println(a);
+}
+}
